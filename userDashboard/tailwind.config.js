a4aa30e1484da37e6dbc5bfbb9a61f363faa51f6
Mona <m.yousefinejad78@gmail.com>
2025-05-28 13:08:59 +0330

    application Published with some assets added

---
+++ userDashboard/tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      colors:{
+        borderColor:"#E2E8F0",
+        secondary:"#F1F5F9"
+      }
+    },
+  }
+}
